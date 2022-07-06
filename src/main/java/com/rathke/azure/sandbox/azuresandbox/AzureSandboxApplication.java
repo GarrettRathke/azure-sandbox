14ed97d387a1fce56e178707dcc425f78804f696
Garrett Rathke <grathke@headstorm.com>
2022-07-06 11:13:01 -0500

    initialized spring boot app

---
+++ src/main/java/com/rathke/azure/sandbox/azuresandbox/AzureSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.rathke.azure.sandbox.azuresandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureSandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureSandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rathke/azure/sandbox/azuresandbox/AzureSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rathke.azure.sandbox.azuresandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureSandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
